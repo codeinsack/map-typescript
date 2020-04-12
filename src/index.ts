cbe9344995e8838c632dd9d7621c8085b9371420
codeinsack <codeinsack@gmail.com>
2020-04-12 21:16:04 +0300

    add marker

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-// import { User } from './User'
+import { User } from './User'
 // import { Company } from './Company'
 import { CustomMap } from './CustomMap'
 //
-// const user = new User()
 // const company = new Company()
-new CustomMap('map')
+const user = new User()
+const customMap = new CustomMap('map')
+
+customMap.addUserMarker(user)
